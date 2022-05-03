71ef6ba83a8f0a667266c1606ad11dc099c03054
unknown <l.boukorras@gmail.com>
2022-05-03 19:13:30 +0200

    branch-firstV2

--- branch-first.bash
+++ branch-first.bash
@@ -2,3 +2,5 @@ git branch
 # ✅ Liste toutes les branches locales dans le dépôt courant
 git branch [nom-de-branche]
 # ✅ Crée une nouvelle branche
+git checkout [nom-de-branche]
+# ✅ Bascule sur la branche spécifiée et met à jour le répertoire de travail
