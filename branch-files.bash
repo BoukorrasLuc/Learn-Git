769c5167984ea9930691994f6756ffde4e9e9b46
unknown <l.boukorras@gmail.com>
2022-05-03 19:32:34 +0200

    branch-filesV6

---
+++ branch-files.bash
@@ -0,0 +1,6 @@
+git rm [fichier]
+# ✅ Supprime le fichier du répertoire de travail et met à jour l'index
+git rm --cached [fichier]
+# ✅ Supprime le fichier du système de suivi de version mais le préserve localement 
+git mv [fichier-nom] [fichier-nouveau-nom]
+# ✅ Renomme le fichier et prépare le changement pour un commit
