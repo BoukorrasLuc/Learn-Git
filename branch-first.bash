62e5e0b10c118a19398614e8d5bc421061402eb5
unknown <l.boukorras@gmail.com>
2022-05-03 19:16:40 +0200

    branch-firstV3

--- branch-first.bash
+++ branch-first.bash
@@ -4,3 +4,9 @@ git branch [nom-de-branche]
 # ✅ Crée une nouvelle branche
 git checkout [nom-de-branche]
 # ✅ Bascule sur la branche spécifiée et met à jour le répertoire de travail
+git checkout -b [nom-de-branche]
+# ✅ Crée une nouvelle branche et bascule sur celle-ci
+git branch -d [nom-de-branche]
+# ✅ Supprime une branche
+git merge [nom-de-branche]
+# ✅ Combine dans la branche courante l'historique de la branche spécifiée
